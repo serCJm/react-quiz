709e75263e7d3a3921890b407d670992af652151
cjmoro <scjmoro@gmail.com>
2020-07-16 19:08:22 -0700

    Add internal state and pass it to QuestionCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import QuestionCard from "./components/QuestionCard";
+
+const TOTAL_QUESTIONS = 0;
 
 function App() {
-	return <div className="App">Quiz</div>;
+	const [loading, setLoading] = useState(false);
+	const [questions, setQuestions] = useState([]);
+	const [number, setNumber] = useState(0);
+	const [userAnswers, setUserAnswers] = useState([]);
+	const [score, setScore] = useState(0);
+	const [gameOver, setGameOver] = useState(true);
+
+	async function startTrivia() {}
+
+	function checkAnswer(e: React.MouseEvent<HTMLButtonElement>) {}
+
+	function nextQuestion() {}
+
+	return (
+		<div className="App">
+			<h1>REACT QUIZ</h1>
+			<button className="start" onClick={startTrivia}>
+				Start
+			</button>
+			<p className="score">Score:</p>
+			<p>Loading Questions ...</p>
+			<QuestionCard
+				questionNumber={number + 1}
+				totalQuestions={TOTAL_QUESTIONS}
+				question={questions[number].question}
+				answers={questions[number].answers}
+				userAnswer={userAnswers ? userAnswers[number] : undefined}
+				callback={checkAnswer}
+			></QuestionCard>
+			<button className="next" onClick={nextQuestion}>
+				Next Question
+			</button>
+		</div>
+	);
 }
 
 export default App;
